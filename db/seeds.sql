54f5e58376b6e697ee09b7cc326672e947e81510
Sadie Sial <sadiesial@gmail.com>
2021-10-15 19:21:40 -0500

    continued roles in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,5 @@
 INSERT INTO department (name)
-VALUE ('Orthopedics'), ('Research Administration'), ('Oncology'), ('Neurology'), ('Radiology')
+VALUE ('Research Administration'), ('Orthopedics'), ('Oncology'), ('Neurology'), ('Radiology');
+
+INSERT INTO role (title, department_id, salary)
+VALUE ('Surgeon', '200,000' 1), ('Nurse', '', 2),  ('MA', '', 3),  ('Clinical Researcher', '', 4),  ('', '', 5),   ('Admin', '', 6);
