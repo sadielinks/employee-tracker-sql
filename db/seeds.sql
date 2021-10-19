e1b383fe870a861f8fb0ddbf6811f584fcb7275c
Sadie Sial <sadiesial@gmail.com>
2021-10-18 20:45:13 -0500

    added employee/value to seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,8 @@
 INSERT INTO department (name)
-VALUE ('Research Administration'), ('Orthopedics'), ('Oncology'), ('Neurology'), ('Radiology');
+VALUE ('Surgery'), ('Nursing'), ('Research'), ('Legal'), ('Administration');
 
 INSERT INTO role (title, department_id, salary)
-VALUE ('Surgeon', '200,000' 1), ('Nurse', '', 2),  ('MA', '', 3),  ('Clinical Researcher', '', 4),  ('', '', 5),   ('Admin', '', 6);
+VALUE ('Surgeon', '200,000' 1), ('Nurse', '65,000', 2),  ('Clinical Researcher', '72,000', 3),  ('Lawyer', '100,000', 4),  ('Administrator', '60,000', 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ('Sal', 'Khan', 200,000, 1, 1), ('Meg', 'Voy', 65,000, 1, 1), ('Sadie', 'Sial', 72,000, 3, 3), ('Mel', 'Smith', 100,000, 4), ('Josh', 'Barr', 60,000, 5);
