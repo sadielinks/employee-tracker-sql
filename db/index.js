adb17190a6c8cb7b7afdfb402ee6991a9bd14596
Sadie Sial <sadiesial@gmail.com>
2021-10-18 22:33:50 -0500

    added classes/constructor

--- db/index.js
+++ db/index.js
@@ -1,14 +1,31 @@
 // this will direct traffic
-const { connect } = require('./connection');
-const connection = require('./connection')
+const { connect } = require("./connection");
+const connection = require("./connection");
 
 // queries - dept, salaries, each data search
 class DB {
-    constructor (connection){
-        this.connection = connection;
-    }
+  constructor(connection) {
+    this.connection = connection;
+  }
 }
 
-module.exports = new DB (connection)
-//OUTSIDE const INSIDE class
-// You might want to use a separate file that contains functions for performing specific SQL queries you'll need to use. A constructor function or class could be helpful for organizing these. You might also want to include a `seeds.sql` file to pre-populate your database, making the development of individual features much easier.
+class Employee {
+  constructor() {
+    this.department;
+    this.department_id;
+    this.title;
+    this.first_name;
+    this.last_name;
+    this.salary;
+    this.role_id;
+    this.manager_id;
+  }
+}
+
+module.exports = new DB(connection);
+// OUTSIDE const INSIDE class
+// You might want to use a separate file that contains 
+// functions for performing specific SQL queries you'll need to use. 
+// A constructor function or class could be helpful for organizing these. 
+// You might also want to include a `seeds.sql` file to pre-populate your database, 
+// making the development of individual features much easier.
