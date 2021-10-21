b3228417d018ef3684837ed053f212ff84f0e536
Sadie Sial <sadiesial@gmail.com>
2021-10-20 23:09:32 -0500

    re-form. the seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,22 @@
 USE employee_db;
 
 INSERT INTO department (department_name)
-VALUES ("Surgery"), ("Nursing"), ("Research"), ("Legal"), ("Administration");
+VALUES ("Surgery"),
+("Nursing"),
+("Research"),
+("Legal"),
+("Administration");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Surgeon", 200000, 1), ("Nurse", 65000, 2),  ("Clinical Researcher", 72000, 3),  ("Lawyer", 100000, 4),  ("Administrator", 60000, 5);
+VALUES ("Surgeon", 200000, 1),
+("Nurse", 65000, 2),
+("Clinical Researcher", 72000, 3),
+("Lawyer", 100000, 4),
+("Administrator", 60000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Sal", "Khan", 1, 1), ("Meg", "Voy", 2, 2), ("Sadie", "Sial", 3, 3), ("Mel", "Smith", 4, 4), ("Josh", "Barr", 5, 5);
+VALUES ("Sal", "Khan", 1, 1),
+("Meg", "Voy", 2, 2),
+("Sadie", "Sial", 3, null),
+("Mel", "Smith", 4, 4),
+("Josh", "Barr", 5, 5);
